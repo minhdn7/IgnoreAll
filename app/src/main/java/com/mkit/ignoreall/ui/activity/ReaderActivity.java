c5d9831e47f6b47d2eaf407485be384872bdeb51
minhdn7 <minhdn231@gmail.com>
2018-06-04 15:58:20 +0700

    update 6/4/2018 tích hợp folio reader lib modify

---
+++ app/src/main/java/com/mkit/ignoreall/ui/activity/ReaderActivity.java
@@ -0,0 +1,15 @@
+package com.mkit.ignoreall.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mkit.ignoreall.R;
+
+public class ReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reader);
+    }
+}
